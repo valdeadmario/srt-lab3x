bc5e2bf9df5c40eaf098b081b7610ffb043489a0
valdeadmario <vladimir.vstup@gmail.com>
2020-04-11 15:17:00 +0300

    Fix algho

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -2,7 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 
 import TabBarIcon from "../components/TabBarIcon";
-import { Perceptron } from "../screens/HomeScreen";
+import { Perceptron } from "../screens/Perceptron";
 import LinksScreen from "../screens/LinksScreen";
 import { Ferma } from "../screens/Ferma";
 
--- screens/Perceptron.js
+++ screens/Perceptron.js
@@ -1,12 +1,62 @@
 import React, { useState } from "react";
 
-import { Text, View, TouchableHighlight, Button, Picker } from "react-native";
+import { Text, View, Button, Picker } from "react-native";
 
 import { styles } from "../styles";
 
-import { calculate, parseResult } from "./functions";
-
 import { Card, Subheader } from "react-native-material-ui";
+
+const calculate = (points, speed, timeDeadline, iterationDeadLine, P) => {
+  let status = false;
+  let iterations = 0;
+  let startTime = Date.now();
+  let y = 0;
+  let delta = 0;
+  let w1 = 0;
+  let w2 = 0;
+
+  let dotIndex = -1;
+
+  while (
+    iterations++ < iterationDeadLine &&
+    Date.now() - startTime < timeDeadline * 1000
+  ) {
+    dotIndex = dotIndex == 3 ? -1 : dotIndex;
+    dotIndex++;
+
+    y = points[dotIndex][0] * w1 + points[dotIndex][1] * w2;
+
+    if (checkWeights(P, points, w1, w2)) {
+      status = true;
+      break;
+    }
+
+    delta = P - y;
+    w1 += delta * points[dotIndex][0] * speed;
+    w2 += delta * points[dotIndex][1] * speed;
+  }
+  const time = Date.now() - startTime;
+  if (status) {
+    return ["Success", iterations, time, w1, w2];
+  }
+
+  return [
+    iterations >= iterationDeadLine
+      ? "Out of iteration boundaries"
+      : "Teaching time has expired",
+    iterations,
+    time,
+  ];
+};
+
+const checkWeights = (P, points, w1, w2) => {
+  return (
+    P < points[0][0] * w1 + points[0][1] * w2 &&
+    P < points[1][0] * w1 + points[1][1] * w2 &&
+    P > points[2][0] * w1 + points[2][1] * w2 &&
+    P > points[3][0] * w1 + points[3][1] * w2
+  );
+};
 
 export const Perceptron = () => {
   const points = [
